d4576f5356841be6ce86ed95b554c1b86e111f5e
Marcin Barcz <marcinbarcz@Marcins-MacBook-Pro.local>
2023-04-18 12:57:21 +0200

    ensure responses arent cached

--- app.py
+++ app.py
@@ -18,3 +18,11 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///expense_tracker.db")
 
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
