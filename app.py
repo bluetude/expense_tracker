d8d0c67237c238f5ad08350d159fb4f5e4bcc0d3
Marcin Barcz <marcinbarcz@Marcins-MacBook-Pro.local>
2023-04-18 12:55:18 +0200

    linked db

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+# Configure application
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///expense_tracker.db")
 
